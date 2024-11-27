58c5b8cd5bb76b8afa26597c0f895e1c8a3bebc4
JOYRU <joycseru@gmail.com>
2024-11-27 13:06:05 +0600

    some changes

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 // import { useAuth } from '../context/AuthContext';
-import { useAuth } from '../context/authContext';
+import { useAuth } from '../context/AuthContext';
 const Register = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
